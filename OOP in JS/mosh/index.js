5a1c5a6c1c66ef6a6511a980079e72d6be9302b3
Emediong Bassey Edem <edememediong1@gmail.com>
2025-02-25 01:25:52 +0100

    constructor property

--- OOP in JS/mosh/index.js
+++ OOP in JS/mosh/index.js
@@ -36,4 +36,40 @@ circle.draw();
     }
  }
 
- const another =  Circle(1); //new keyword creates an empty object, sets "this" to point to that object and returns the object from the function
+ const another =  new Circle(1); //new keyword creates an empty object, sets "this" to point to that object and returns the object from the function
+
+ console.log(another.constructor);
+//Every object in JS has a constructor property that references the function that was used to create that object
+//In this case, another.constructor will return a reference to the Circle function
+
+/*
+    For example an object created using the object literal syntax will have a constructor property that references the Object function
+
+    const x = {};
+    x.constructor; //returns Object() { [native code] }
+
+    You can also create objects using the Object() constructor function
+    const y = new Object();
+    y.constructor; //returns Object() { [native code] }
+
+    Other constructor functions include:
+    new String(); //returns a string object 
+    new Number(); //returns a number object
+    new Boolean(); //returns a boolean object
+    new Function(); //returns a function object
+    new Array(); //returns an array object
+    new RegExp(); //returns a regular expression object
+    new Date(); //returns a date object
+    new Error(); //returns an error object
+    new Symbol(); //returns a symbol object
+    new Map(); //returns a map object
+    new Set(); //returns a set object
+    new WeakMap(); //returns a weakmap object
+    new WeakSet(); //returns a weakset object
+    new ArrayBuffer(); //returns an arraybuffer object
+
+    The constructor property is useful for checking the type of an object
+    For example, you can use the instanceof operator to check if an object is an instance of a specific constructor function
+    const x = {};
+    x instanceof Object; //returns true
+*/
